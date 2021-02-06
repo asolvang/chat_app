89efe9e14a5881d1af5de8aeb3ab3772beb138b4
asolvang <andersfido@gmail.com>
2021-02-06 23:53:38 +0100

    Finished for the night Completed Devise forms, and the aside menu.
    Next step - adding messages.

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -2,8 +2,26 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['account', 'messages']
 
-  visible() {
-    console.log(fetch(this.data))
-    
+  account() {
+    hiddenToggler(this.accountTarget)
   }
+
+  messages() {
+    hiddenToggler(this.messagesTarget);
+  }
+
 }
+
+
+// Reusable helper function
+function hiddenToggler(el){
+  el.classList.toggle("hidden")
+  event.currentTarget.classList.toggle("bg-blue-500")
+  event.currentTarget.classList.toggle("text-white")
+  event.currentTarget.classList.toggle("text-blue-600")
+  
+  event.currentTarget.classList.toggle("hover:bg-blue-600")
+  event.currentTarget.classList.toggle("hover:bg-blue-200")
+  event.currentTarget.classList.toggle("hover:text-blue-600")
+  event.currentTarget.classList.toggle("hover:text-white")
+} 
