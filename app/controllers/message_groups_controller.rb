bb0df631d66755e8e9a19eee832bce31b96b4268
asolvang <andersfido@gmail.com>
2021-02-15 21:39:33 +0100

    Added devise failure messages

--- app/controllers/message_groups_controller.rb
+++ app/controllers/message_groups_controller.rb
@@ -1,4 +1,5 @@
 class MessageGroupsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_message_group, only: %i[ show edit update destroy ]
 
   # GET /message_groups or /message_groups.json
